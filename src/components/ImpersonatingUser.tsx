a728c0475623c6d22fd9a5e5e7638f5bba4579c8
Akash layal <akash.layal.web@gmail.com>
2023-12-14 13:17:18 +0530

    fix: impersonating users

--- src/components/ImpersonatingUser.tsx
+++ src/components/ImpersonatingUser.tsx
@@ -1,11 +1,10 @@
 "use client"
 import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
-import { FC } from 'react'
-
 import { Session } from "next-auth"
 import { useSession } from 'next-auth/react'
 import Image from "next/image"
-import { useRouter } from "next/navigation"
+import { FC } from 'react'
+import Draggable from "react-draggable"
 import { toast as hotToast } from "react-hot-toast"
 import { Icons } from "./Icons"
 import { Avatar, AvatarFallback } from './ui/avatar'
@@ -17,8 +16,6 @@ interface ImpersonatingUserProps {
 
 const ImpersonatingUser: FC<ImpersonatingUserProps> = ({ }) => {
   const { data: session, status, update } = useSession()
-  const router = useRouter()
-
   const user = session?.user
   if (!session?.user.isImpersonating || !session.user.actor) {
     return null
@@ -93,49 +90,58 @@ const ImpersonatingUser: FC<ImpersonatingUserProps> = ({ }) => {
 
     }
   }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger
-        asChild
-        className='overflow-visible fixed top-52 !right-5'>
-        <Button className='rounded-full w-10 h-10 aspect-square bg-red-500 hover:bg-red-700'>
-          <Avatar className='relative  flex justify-center items-center'>
-            <ExclamationTriangleIcon className="h-6 w-6" />
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
+    <>
 
-      <DropdownMenuContent className='bg-white ' align='end'>
-        <div className='flex items-center justify-start gap-2 p-2'>
-          <div className='flex flex-col space-y-0.5 leading-none'>
-            {user?.actor && (
-              <>
-                <p className='font-medium text-sm  text-red-500'> Signed in as </p>
-                <p className='font-medium text-sm text-black'>
-                  {user.actor.actorName}
-                </p>
-              </>
+      <Draggable>
+        <DropdownMenu >
 
-            )}
-            {user?.isImpersonating && user?.actor.actorEmail && (
-              <p className='w-[200px] truncate text-xs text-zinc-700'>
-                {user.actor.actorEmail}
-              </p>
-            )}
-          </div>
-        </div>
+          <DropdownMenuTrigger
+            asChild
+            className='overflow-visible fixed top-32 right-6'>
+            <Button className='rounded-full w-10 h-10 aspect-square bg-red-500 hover:bg-red-700'>
+              <Avatar className='relative  flex justify-center items-center'>
+                <ExclamationTriangleIcon className="h-6 w-6" />
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
 
-        <DropdownMenuSeparator />
+          <DropdownMenuContent className='bg-white ' align='end'>
+            <div className='flex items-center justify-start gap-2 p-2'>
+              <div className='flex flex-col space-y-0.5 leading-none'>
+                {user?.actor && (
+                  <>
+                    <p className='font-medium text-sm  text-red-500'> Signed in as </p>
+                    <p className='font-medium text-sm text-black'>
+                      {user.actor.actorName}
+                    </p>
+                  </>
 
-        <DropdownMenuItem className='cursor-pointer' onClick={discardImpersonation}>
-          Log out impersonated session
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+                )}
+                {user?.isImpersonating && user?.actor.actorEmail && (
+                  <p className='w-[200px] truncate text-xs text-zinc-700'>
+                    {user.actor.actorEmail}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem className='cursor-pointer' onClick={discardImpersonation}>
+              Log out impersonated session
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+
+        </DropdownMenu>
+      </Draggable >
+
+
+    </>
+
 
   )
-
-
 }
 
 export default ImpersonatingUser
