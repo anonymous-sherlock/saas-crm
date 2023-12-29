4331e71b830adf03e44da823506712fdce2cc3b0
Akash layal <akash.layal.web@gmail.com>
2023-12-29 20:10:15 +0530

    fix: mispledd fixed

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -14,7 +14,7 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: z.string().min(1),
 
     NEXTAUTH_SECRET: z.string().min(1),
-    NENEXTAUTH_URL: z.string().min(1),
+    NEXTAUTH_URL: z.string().min(1),
 
     CDN_API_KEY: z.string().min(1),
     CDN_UPLOAD_URL: z.string().url(),
@@ -54,7 +54,7 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 
-    NENEXTAUTH_URL: process.env.NENEXTAUTH_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 
     RESEND_API_KEY: process.env.RESEND_API_KEY,
